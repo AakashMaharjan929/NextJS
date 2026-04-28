10999e237632d8dd8b448d3a270b8548f97825ef
AakashMaharjan929 <aakashmaharjan2222@gmail.com>
2026-04-28 14:09:05 -0700

    Updated Admin Page with Finance Chart and Event Calendar components. Adjusted layout for better responsiveness and user experience.

--- school-management-system/src/app/(dashboard)/layout.tsx
+++ school-management-system/src/app/(dashboard)/layout.tsx
@@ -8,7 +8,7 @@ export default function DashboardLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className="h-screen flex">
+  return <div className="h-screen flex overflow-hidden">
     {/* LEFT */}
     <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
       <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
@@ -18,7 +18,7 @@ export default function DashboardLayout({
       <Menu/>
     </div>  
     {/* RIGHT */}
-    <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+    <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-y-auto">
       <Navbar/>
       {children}
     </div>
