4991afb60bc6fa22cdfdb8bb538aa0dcc163f20c
AakashMaharjan929 <aakashmaharjan2222@gmail.com>
2026-05-09 05:50:14 -0700

    The Lessons, Classes, Exams, Assignments, and Results pages have been updated to show data from the database. The links to students page have been updated to pass classId and studentId.

--- school-management-system/src/app/(dashboard)/list/parents/page.tsx
+++ school-management-system/src/app/(dashboard)/list/parents/page.tsx
@@ -51,7 +51,7 @@ const columns = [
 const renderRow = (item: ParentList) => (
   <tr
     key={item.id}
-    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-AakashPurpleLight"
   >
     <td className="flex items-center gap-4 p-4">
       <div className="flex flex-col">
@@ -119,10 +119,10 @@ const renderRow = (item: ParentList) => (
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-AakashYellow">
               <Image src="/filter.png" alt="" width={14} height={14} />
             </button>
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-AakashYellow">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
             {/* {role === "admin" && <FormContainer table="parent" type="create" />} */}
