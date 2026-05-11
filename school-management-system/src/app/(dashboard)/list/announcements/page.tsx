5cf89a8475b22072f0d5d66335b87562ac09c320
AakashMaharjan929 <aakashmaharjan2222@gmail.com>
2026-05-11 06:09:50 -0700

    -Stable version

--- school-management-system/src/app/(dashboard)/list/announcements/page.tsx
+++ school-management-system/src/app/(dashboard)/list/announcements/page.tsx
@@ -4,21 +4,21 @@ import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
 import {prisma} from "@/lib/prsima";
 import { ITEM_PER_PAGE } from "@/lib/settings";
-import { Class, Event, Prisma } from "@prisma/client";
+import { Announcement, Class, Prisma } from "@prisma/client";
 import Image from "next/image";
 import { auth } from "@clerk/nextjs/server";
 
-type EventList = Event & { class: Class };
 
-const EventListPage = async (props: {
+type AnnouncementList = Announcement & { class: Class };
+const AnnouncementListPage = async (props: {
   searchParams: Promise<{ [key: string]: string | undefined }>;
 }) => {
   const searchParams = await props.searchParams;
-
+  
   // const { userId, sessionClaims } = auth();
   // const role = (sessionClaims?.metadata as { role?: string })?.role;
   // const currentUserId = userId;
-
+  
   const columns = [
     {
       header: "Title",
@@ -33,16 +33,6 @@ const EventListPage = async (props: {
       accessor: "date",
       className: "hidden md:table-cell",
     },
-    {
-      header: "Start Time",
-      accessor: "startTime",
-      className: "hidden md:table-cell",
-    },
-    {
-      header: "End Time",
-      accessor: "endTime",
-      className: "hidden md:table-cell",
-    },
     // ...(role === "admin"
     //   ? [
     //       {
@@ -52,8 +42,8 @@ const EventListPage = async (props: {
     //     ]
     //   : []),
   ];
-
-  const renderRow = (item: EventList) => (
+  
+  const renderRow = (item: AnnouncementList) => (
     <tr
       key={item.id}
       className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
@@ -61,42 +51,27 @@ const EventListPage = async (props: {
       <td className="flex items-center gap-4 p-4">{item.title}</td>
       <td>{item.class?.name || "-"}</td>
       <td className="hidden md:table-cell">
-        {new Intl.DateTimeFormat("en-US").format(item.startTime)}
-      </td>
-      <td className="hidden md:table-cell">
-        {item.startTime.toLocaleTimeString("en-US", {
-          hour: "2-digit",
-          minute: "2-digit",
-          hour12: false,
-        })}
-      </td>
-      <td className="hidden md:table-cell">
-        {item.endTime.toLocaleTimeString("en-US", {
-          hour: "2-digit",
-          minute: "2-digit",
-          hour12: false,
-        })}
+        {new Intl.DateTimeFormat("en-US").format(item.date)}
       </td>
       <td>
         <div className="flex items-center gap-2">
           {/* {role === "admin" && (
             <>
-              <FormContainer table="event" type="update" data={item} />
-              <FormContainer table="event" type="delete" id={item.id} />
+              <FormContainer table="announcement" type="update" data={item} />
+              <FormContainer table="announcement" type="delete" id={item.id} />
             </>
           )} */}
         </div>
       </td>
     </tr>
   );
-
   const { page, ...queryParams } = searchParams;
 
   const p = page ? parseInt(page) : 1;
 
   // URL PARAMS CONDITION
 
-  const query: Prisma.EventWhereInput = {};
+  const query: Prisma.AnnouncementWhereInput = {};
 
   if (queryParams) {
     for (const [key, value] of Object.entries(queryParams)) {
@@ -120,15 +95,15 @@ const EventListPage = async (props: {
   //   parent: { students: { some: { parentId: currentUserId! } } },
   // };
 
-    // query.OR = [
-    //   { classId: null },
-    //   {
-    //     class: roleConditions[role as keyof typeof roleConditions] || {},
-    //   },
-    // ];
+  // query.OR = [
+  //   { classId: null },
+  //   {
+  //     class: roleConditions[role as keyof typeof roleConditions] || {},
+  //   },
+  // ];
 
   const [data, count] = await prisma.$transaction([
-    prisma.event.findMany({
+    prisma.announcement.findMany({
       where: query,
       include: {
         class: true,
@@ -136,14 +111,16 @@ const EventListPage = async (props: {
       take: ITEM_PER_PAGE,
       skip: ITEM_PER_PAGE * (p - 1),
     }),
-    prisma.event.count({ where: query }),
+    prisma.announcement.count({ where: query }),
   ]);
 
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Events</h1>
+        <h1 className="hidden md:block text-lg font-semibold">
+          All Announcements
+        </h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
@@ -153,7 +130,9 @@ const EventListPage = async (props: {
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
-            {/* {role === "admin" && <FormContainer table="event" type="create" />} */} 
+            {/* {role === "admin" && (
+              <FormContainer table="announcement" type="create" />
+            )} */}
           </div>
         </div>
       </div>
@@ -165,4 +144,4 @@ const EventListPage = async (props: {
   );
 };
 
-export default EventListPage;
+export default AnnouncementListPage;
